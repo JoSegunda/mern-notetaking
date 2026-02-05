64bdb20c0b42332cdd0f8acac42883532512653a
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-02-05 22:27:35 +0000

    incluir notas

--- backend/routes/notesRoute.js
+++ backend/routes/notesRoute.js
@@ -4,17 +4,17 @@ const router = express.Router();
 
 // Isto é  uma rota(route) para buscar as notas
 router.get("/", (req, resp) => {
-    resp.status(200).json({message: "You have 10 notes!"})
+    resp.status(200).send("Fetching the notes")
 })
 
 // Isto é uma rota para salvar as notas
 router.post("/", (req, resp) => {
-    resp.status(200).json({message: "Note posted successfully"})
+    resp.status(201).json({message: "Note posted successfully"})
 })
 
 // Isto é uma rota para atualizar as notas
 // O Id tem a ver com que nota queremos atualizar
-// Ex: // http://localhost:5001/api/notes/22
+// Ex:http://localhost:5001/api/notes/22
 router.put("/id:", (req, resp) => {
     resp.status(200).json({message: "Note updated successfully"})
 })
