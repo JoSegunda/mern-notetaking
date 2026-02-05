ea0fb8d2247b579f4334faabbc6ed36643c302ae
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-02-05 22:03:56 +0000

    adds routes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,27 @@ const express = require("express")
 
 const app = express();
 
+// Isto é  uma rota(route)
 app.get("/api/notes", (req, resp) => {
     resp.status(200).send("11 notes")
 })
 
+// put something to DB
+app.post("/api/notes", (req, resp) => {
+    resp.status(201).json({message:"Node posted successfully"})
+})
+
+// update
+app.put("/api/notes/:id", (req, resp) => {
+    resp.status(201).json({message:"Note updated successfully"})
+})
+// delete
+// The id is for the note we want to edit
+app.delete("/api/notes/:id", (req, resp) => {
+    resp.status(201).json({message:"Note deleted successfully"})
+})
+
+// http://localhost:5001/api/notes/22
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
 })
