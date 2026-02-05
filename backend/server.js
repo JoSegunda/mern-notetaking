1370c9f096913c5da604db0c2c6a5e8070678060
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-02-05 22:16:41 +0000

    deletes comments

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,6 @@ const app = express();
 
 app.use("/api/notes");
 
-// // Isto é  uma rota(route)
-// app.get("/api/notes", (req, resp) => {
-//     resp.status(200).send("11 notes")
-// })
-
-// // put something to DB
-// app.post("/api/notes", (req, resp) => {
-//     resp.status(201).json({message:"Node posted successfully"})
-// })
-
-// // update
-// app.put("/api/notes/:id", (req, resp) => {
-//     resp.status(201).json({message:"Note updated successfully"})
-// })
-// // delete
-// // The id is for the note we want to edit
-// app.delete("/api/notes/:id", (req, resp) => {
-//     resp.status(201).json({message:"Note deleted successfully"})
-// })
-
-// http://localhost:5001/api/notes/22
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
 })
